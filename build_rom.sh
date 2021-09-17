3508a62a8a46b9c6b6c28270b8feeb128ef4a7c1
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-17 13:25:50 +0530

    Fix kernel config

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch legion_X00TD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Kolkata #put before last build comman
+export TZ=Asia/Kolkata #put before last build command
 make legion
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
